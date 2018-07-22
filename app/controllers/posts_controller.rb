159126718925d611b3507f8ea45d50f616b2af5b
asad451 <asadmasood451@gmail.com>
2018-07-22 20:36:37 +0500

    Dev Change

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,18 @@
+class PostsController < ApplicationController
+  
+  def index
+    @posts = Post.all
+  end
+  
+  def new
+    @post = Post.new
+  end
+  
+  def create
+    if @post.save
+      redirect_to root_path
+    else
+      render "new"
+    end
+  end
+end
